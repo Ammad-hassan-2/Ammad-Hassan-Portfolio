0ed2818e95b5f6bf46d8163022973846f3ca1734
Ammad-hassan-2 <amm.hassan01@gmail.com>
2024-11-22 02:14:06 +0500

    Update research.js

--- assets/js/research.js
+++ assets/js/research.js
@@ -4,22 +4,147 @@ const researchTable = document.querySelector(".main");
 
 const research = [
   {
-    title: "An Enhanced Hybrid Diagnostic Deep Learning Framework using Ensemble Vit-ResNets for Oral Carcinoma Detection",
+    title: "A Batch Normalized Inference Network Keeps the KL Vanishing Away",
     authors:
-      "Ammad Hassan, Glen Shaji, F. Mohammed, Sai Konkimalla",
+      "Qile Zhu, Wei Bi, Xiaojiang Liu, Xiyao Ma, Xiaolin Li and Dapeng Wu",
     conferences:
-      "Seminar on Molecular Imaging Techniques, Mini-fellowship Program, Stanford Medicine(Best Research Award), International Young Researcher's Conference (IYRC) at Vagelos College of Physicians and Surgeons, Columbia University, Case Study Presentations at Harvard VISION Global Health & Leadership Conference, 5th Annual MehtA+ Machine Learning Conference In the process of publishing in the Columbia Junior Science Journal",
-    researchYr: 2024,
+      "The 58th Annual Meeting of the Association for Computational Linguistics",
+    researchYr: 2020,
     citebox: "popup1",
-    image: "assets/images/research-page/paper.png",
+    image: "assets/images/research-page/inteferenceNetwork.png",
     citation: {
       vancouver:
-        "Ammad Hassan, Glen Shaji, F. Mohammed, Sai Konkimalla. An Enhanced Hybrid Diagnostic Deep Learning Framework using Ensemble Vit-ResNets for Oral Carcinoma Detection. Seminar on Molecular Imaging Techniques, Stanford Medicine. IYRC, Columbia. Harvard VISION Global Health Conference.",
+        "Qile Zhu, Wei Bi, Xiaojiang Liu, Xiyao Ma, Xiaolin Li and Dapeng Wu. A Batch Normalized Inference Network Keeps the KL Vanishing Away. The 58th Annual Meeting of the Association for Computational Linguistics 2020.",
     },
     abstract:
-      "Oral cancer detection can be approached using two distinct methods: a fast ‘System-1’ approach that directly applies diagnostic models without extensive data processing, and a slower ‘System2’ approach that involves a detailed analysis. While System-2 approaches generally offer greater accuracy, they are often computationally expensive and may not be feasible with limited datasets or resources. Moreover, relying solely on either System-1 or System-2 methods overlooks the specific needs and constraints of different diagnostic scenarios. To address these challenges, we propose the Hybrid Meta-Diagnostic Framework, which balances between System-1 and System-2 methods to enhance oral carcinoma detection. This framework includes: (i) fine-tuning pre-trained Vision Transformer and Swin Transformer models on images, (ii) fine-tuning pre-trained ResNet-18 and MobileNETV2 models on images, (iii) using evaluation metrics to identify the best transformer and CNN architectures, and (iv) ensembling these models using the soft voting technique to combine predictions. The system dynamically combines these methods based on the complexity of the diagnostic task, optimizing performance and resource use. We fine-tuned and ensembled the models on a single dataset, requiring only standard pre-processing and fine-tuning techniques. Experimental results show that our Hybrid Diagnostic Framework outperforms traditional single-model approaches, demonstrating key advantages: (1) adaptability by balancing between fast and detailed analyses, (2) integration of diverse model architectures for improved diagnostic accuracy, and (3) robustness in handling small, resource-limited datasets.",
+      "This is currently left empty and this can be considered as a dummy data 1",
     absbox: "absPopup1",
- 
+  },
+
+  {
+    title: "A Call for More Rigor in Unsupervised Cross-lingual Learning",
+    authors:
+      "Mikel Artetxe, Sebastian Ruder, Dani Yogatama, Gorka Labaka and Eneko Agirre",
+    conferences:
+      "The 58th Annual Meeting of the Association for Computational Linguistics",
+    researchYr: 2020,
+    citebox: "popup2",
+    image: "assets/images/research-page/crossLingual.png",
+    citation: {
+      vancouver:
+        "Mikel Artetxe, Sebastian Ruder, Dani Yogatama, Gorka Labaka and Eneko Agirre. A Call for More Rigor in Unsupervised Cross-lingual Learning. The 58th Annual Meeting of the Association for Computational Linguistics 2020.",
+    },
+    abstract:
+      "This is currently left empty and this can be considered as a dummy data 2",
+    absbox: "absPopup2",
+  },
+
+  {
+    title:
+      "A Comprehensive Analysis of Preprocessing for Word Representation Learning in Affective Tasks",
+    authors: "Nastaran Babanejad, Ameeta Agrawal, Aijun An and Manos Papagelis",
+    conferences:
+      "The 58th Annual Meeting of the Association for Computational Linguistics",
+    researchYr: 2020,
+    citebox: "popup3",
+    image: "assets/images/research-page/wordRepresentation.png",
+    citation: {
+      vancouver:
+        "Nastaran Babanejad, Ameeta Agrawal, Aijun An and Manos Papagelis. A Comprehensive Analysis of Preprocessing for Word Representation Learning in Affective Tasks. The 58th Annual Meeting of the Association for Computational Linguistics 2020.",
+    },
+    abstract:
+      "This is currently left empty and this can be considered as a dummy data 3",
+    absbox: "absPopup3",
+  },
+
+  {
+    title:
+      "A Contextual Hierarchical Attention Network with Adaptive Objective for Dialogue State Tracking",
+    authors:
+      "Yong Shan, Zekang Li, Jinchao Zhang, Fandong Meng, Yang Feng, Cheng Niu and Jie Zhou",
+    conferences:
+      "The 58th Annual Meeting of the Association for Computational Linguistics",
+    researchYr: 2020,
+    citebox: "popup4",
+    image: "assets/images/research-page/dialogueState.png",
+    citation: {
+      vancouver:
+        "Yong Shan, Zekang Li, Jinchao Zhang, Fandong Meng, Yang Feng, Cheng Niu and Jie Zhou. A Contextual Hierarchical Attention Network with Adaptive Objective for Dialogue State Tracking. The 58th Annual Meeting of the Association for Computational Linguistics 2020.",
+    },
+    abstract:
+      "This is currently left empty and this can be considered as a dummy data 4",
+    absbox: "absPopup4",
+  },
+
+  {
+    title: "Dual Super-Resolution Learning for Semantic Segmentation",
+    authors: "Wang, Li and Li, Dong and Zhu, Yousong and Tian, Lu and Shan, Yi",
+    conferences:
+      "IEEE/CVF Conference on Computer Vision and Pattern Recognition (CVPR)",
+    researchYr: 2020,
+    citebox: "popup5",
+    image: "assets/images/research-page/semanticSegmentation.png",
+    citation: {
+      vancouver:
+        "Wang, Li and Li, Dong and Zhu, Yousong and Tian, Lu and Shan, Yi. Dual Super-Resolution Learning for Semantic Segmentation. IEEE/CVF Conference on Computer Vision and Pattern Recognition (CVPR) 2020.",
+    },
+    abstract:
+      "This is currently left empty and this can be considered as a dummy data 5",
+    absbox: "absPopup5",
+  },
+
+  {
+    title: "Deep Unfolding Network for Image Super-Resolution",
+    authors: "Zhang, Kai and Van Gool, Luc and Timofte, Radu",
+    conferences:
+      "IEEE/CVF Conference on Computer Vision and Pattern Recognition (CVPR)",
+    researchYr: 2020,
+    citebox: "popup6",
+    image: "assets/images/research-page/deepNetwork.png",
+    citation: {
+      vancouver:
+        "Zhang, Kai and Van Gool, Luc and Timofte, Radu. Deep Unfolding Network for Image Super-Resolution. IEEE/CVF Conference on Computer Vision and Pattern Recognition (CVPR) 2020.",
+    },
+    abstract:
+      "This is currently left empty and this can be considered as a dummy data 6",
+    absbox: "absPopup6",
+  },
+
+  {
+    title:
+      "Unsupervised Learning for Intrinsic Image Decomposition From a Single Image",
+    authors: "Liu, Yunfei and Li, Yu and You, Shaodi and Lu, Feng",
+    conferences:
+      "IEEE/CVF Conference on Computer Vision and Pattern Recognition (CVPR)",
+    researchYr: 2020,
+    citebox: "popup7",
+    image: "assets/images/research-page/imageDecomposition.png",
+    citation: {
+      vancouver:
+        "Liu, Yunfei and Li, Yu and You, Shaodi and Lu, Feng. Unsupervised Learning for Intrinsic Image Decomposition From a Single Image. IEEE/CVF Conference on Computer Vision and Pattern Recognition (CVPR) 2020.",
+    },
+    abstract:
+      "This is currently left empty and this can be considered as a dummy data 7",
+    absbox: "absPopup7",
+  },
+  {
+    title:
+      "Forward and Backward Information Retention for Accurate Binary Neural Networks",
+    authors:
+      "Qin, Haotong and Gong, Ruihao and Liu, Xianglong and Shen, Mingzhu and Wei, Ziran and Yu, Fengwei and Song, Jingkuan",
+    conferences:
+      "IEEE/CVF Conference on Computer Vision and Pattern Recognition (CVPR)",
+    researchYr: 2020,
+    citebox: "popup8",
+    image: "assets/images/research-page/neuralNetworks.jpg",
+    citation: {
+      vancouver:
+        "Qin, Haotong and Gong, Ruihao and Liu, Xianglong and Shen, Mingzhu and Wei, Ziran and Yu, Fengwei and Song, Jingkuan. Forward and Backward Information Retention for Accurate Binary Neural Networks. IEEE/CVF Conference on Computer Vision and Pattern Recognition (CVPR) 2020.",
+    },
+    abstract:
+      "This is currently left empty and this can be considered as a dummy data 8",
+    absbox: "absPopup8",
+  },
 ];
 AOS.init();
 const fillData = () => {
